6a8bff6a9ae53d296ca7989712798bc1e7d8dacf
keidsondesigner <keidsondesigner@gmail.com>
2023-08-24 19:41:36 -0400

    fix(Auth): add feature AuthMiddleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+type TokenPayload = {
+    id: string;
+    iat: number;
+    exp: number;
+}
+
+export function AuthMiddleware(req: Request, res: Response, next: NextFunction ) {
+
+    const { authorization } = req.headers;
+
+    if(!authorization) {
+        return res.status(401).json({error: "Token not provided"});
+    }
+
+    const [, token] = authorization.split(" ");
+    console.log(token);
+
+    try {
+        const decoded = verify(token, "JWT_SECRET");
+        const { id } = decoded as TokenPayload;
+
+        req.userId = id;
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({error: "Token invalid"});
+    }
+}

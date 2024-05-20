fe2f1fe5c2ad8064c4c870907c8d845cb4667bae
Keidson Roby <keidsondesigner@gmail.com>
2024-05-20 06:01:20 -0400

    ajustes dotenv

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,10 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-// console.log('DATABASE_URL:', process.env.DATABASE_URL); // Debugging database URL
-
-require('dotenv').config();
-
 import { PrismaClient } from "@prisma/client";
 
 export const prisma  = new PrismaClient();
